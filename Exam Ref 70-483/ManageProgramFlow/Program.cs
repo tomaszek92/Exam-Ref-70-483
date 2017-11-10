46dc1e82fe304a4edb8de28876b9d1ab80d1f7b8
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2017-11-10 15:46:26 +0100

    Initial commit

---
+++ Exam Ref 70-483/ManageProgramFlow/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ManageProgramFlow
+{
+    static class Program
+    {
+        private static readonly ThreadLocal<int> Field =
+            new ThreadLocal<int>(() => Thread.CurrentThread.ManagedThreadId);
+
+        private static void Main(string[] args)
+        {
+            //Task<int> t = Task.Run(() => 42);
+            //t.ContinueWith(i => { Console.WriteLine("ContinueWith"); });
+            //t.ContinueWith(i => { Console.WriteLine("Canceled"); }, TaskContinuationOptions.OnlyOnCanceled);
+            //t.ContinueWith(i => { Console.WriteLine("Faulted"); }, TaskContinuationOptions.OnlyOnFaulted);
+            //t.ContinueWith(i => { Console.WriteLine("Completed"); }, TaskContinuationOptions.OnlyOnRanToCompletion);
+            //t.Wait();
+            //Console.ReadKey();
+
+            var enumerable = Enumerable.Range(0, 1000);
+            ParallelLoopResult result = Parallel.ForEach(enumerable, (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 500)
+                {
+                    loopState.Break();
+                }
+                return;
+            });
+        }
+
+        private static void ThreadProc(object stateInfo)
+        {
+            // No state object was passed to QueueUserWorkItem, so stateInfo is null.
+            Console.WriteLine("Hello from the thread pool.");
+        }
+    }
+}
